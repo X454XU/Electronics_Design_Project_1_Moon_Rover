9665766f9b9688fe9d6acf4517dfb9b7e737c8b7
justinlam24 <133984960+justinlam24@users.noreply.github.com>
2023-05-30 14:08:16 +0100

    Update polarity.h

--- rover/src/polarity.h
+++ rover/src/polarity.h
@@ -1,1 +1,29 @@
 // Contains functions required to measure polarity of alien's magnetic field
+
+// Hall effect sensor pin
+const int hallSensorPin = 2;
+
+void setup() {
+  // Set the hall sensor pin as input
+  pinMode(hallSensorPin, INPUT);
+  
+  // Start serial communication
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Read the state of the hall sensor pin
+  int hallSensorState = digitalRead(hallSensorPin);
+
+  // Check the polarity based on the sensor state
+  if (hallSensorState == HIGH) {
+    // Polarity is positive
+    Serial.println("Positive polarity");
+  } else {
+    // Polarity is negative
+    Serial.println("Negative polarity");
+  }
+
+  // Delay for a certain duration
+  delay(500);
+}
